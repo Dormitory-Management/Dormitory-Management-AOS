41fcb3c2845825a9f0881350662b5957a0adb484
김보운 <haioer@naver.com>
2021-05-31 09:53:02 +0900

    로그인 수정

--- app/src/main/java/com/bowoon/android/dormitory_management_aos/activities/login/viewmodel/LoginActivityViewModel.kt
+++ app/src/main/java/com/bowoon/android/dormitory_management_aos/activities/login/viewmodel/LoginActivityViewModel.kt
@@ -3,27 +3,32 @@ package com.bowoon.android.dormitory_management_aos.activities.login.viewmodel
 import com.bowoon.android.common.log.Log
 import com.bowoon.android.dormitory_management_aos.base.BaseViewModel
 import com.bowoon.android.dormitory_management_aos.base.dormitoryApi
+import com.bowoon.android.dormitory_management_aos.base.networkConnection
 import com.bowoon.android.dormitory_management_aos.models.LoginData
 import java.net.HttpURLConnection
 
 class LoginActivityViewModel : BaseViewModel() {
     fun doLogin(id: String, password: String, onSuccess: (() -> Unit)?, onError: ((Throwable) -> Unit)?) {
-        dormitoryApi?.doLogin(
-            compositeDisposable,
-            mapOf(),
-            LoginData(id, password),
-            {
-                if (it.state == HttpURLConnection.HTTP_OK) {
-                    Log.d("Login Success")
-                    onSuccess?.invoke()
-                } else if (it.state in 400 .. 499) {
-                    Log.e("Login Failed ${it.state}")
+        if (networkConnection) {
+            dormitoryApi?.doLogin(
+                compositeDisposable,
+                mapOf(),
+                LoginData(id, password),
+                {
+                    if (it.state == HttpURLConnection.HTTP_OK) {
+                        Log.d("Login Success")
+                        onSuccess?.invoke()
+                    } else if (it.state in 400..499) {
+                        Log.e("Login Failed ${it.state}")
+                    }
+                },
+                { e ->
+                    Log.e("Login Failed")
+                    onError?.invoke(e)
                 }
-            },
-            { e ->
-                Log.e("Login Failed")
-                onError?.invoke(e)
-            }
-        )
+            )
+        } else {
+            onSuccess?.invoke()
+        }
     }
 }
